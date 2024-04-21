ae34d2e494b92caffb35f9c5485bafc16b603889
wdevore <william.quartz@gmail.com>
2024-04-21 13:07:39 -0400

    added very first template of basic window required LD_LIBRARY_PATH to work

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # DartOpenGL
 The project is a composition of Dart and OpenGL applications ranging from Neuromorphic to Games
+
+# Create new project
+You can create a new project via Flutter. Using *dart*
+
+```sh
+dart create basic_template
+```
+
+# Json launch entry
+```json
+{
+    "name": "DartRunner",
+    "cwd": "/home/iposthuman/Documents/dart/DartOpenGL/",
+    "program": "basic_template/bin/main.dart",
+    "request": "launch",
+    "type": "dart"
+},
+```
+
+# GLFW
+finding .so library file:
+```sh
+ldconfig -p | grep libglfw
+```
+Produces:
+```
+libglfw.so.3 (libc6,x86-64) => /lib/x86_64-linux-gnu/libglfw.so.3
+```
+
+Next create a link.
+
+Creating link to libglfw3.so:
+```sh
+ln -s /lib/x86_64-linux-gnu/libglfw.so.3 /usr/local/lib/libglfw.so
+```
+
+Then add a path var.
+
+Adding an entry to settings.json didn't seem to work:
+```json
+"dart.env": {
+    "name": "LD_LIBRARY_PATH", "value": "/usr/local/lib/"
+}
+```
+However, adding an entry to my .bashrc file did:
+```sh
+export LD_LIBRARY_PATH="/usr/local/lib"
+```
+And then ```source .bashrc`` and restarting vscode worked.
---
+++ basic_template/bin/main.dart
@@ -0,0 +1,90 @@
+import 'dart:ffi';
+import 'package:glew/glew.dart';
+import 'package:glfw3/glfw3.dart';
+
+// settings
+const gScrWidth = 800;
+const gScrHeight = 600;
+
+int main(List<String> arguments) {
+  // glfw: initialize and configure
+  // ------------------------------
+  if (glfwInit() != GLFW_TRUE) {
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // --------------------
+  // glfw window creation
+  // --------------------
+  var window =
+      glfwCreateWindow(gScrWidth, gScrHeight, 'LearnOpenGL', nullptr, nullptr);
+  if (window == nullptr) {
+    print('Failed to create GLFW window');
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(
+      window, Pointer.fromFunction(framebufferSizeCallback));
+
+  // --------------------
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
+  gladLoadGLLoader(glfwGetProcAddress);
+
+  // -----------
+  // render loop
+  // -----------
+  while (glfwWindowShouldClose(window) == GLFW_FALSE) {
+    // input
+    // -----
+    processInput(window);
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // ------------------------------------------------------------------
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
+  glfwTerminate();
+
+  return 0;
+}
+
+// ---------------------------------------------------------------------------------------------------------
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(Pointer<GLFWwindow> window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+// ---------------------------------------------------------------------------------------------
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebufferSizeCallback(
+    Pointer<GLFWwindow> window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width and
+  // height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
+
+// String _getPath() {
+//   final cjsonExamplePath = Directory.current.absolute.path;
+//   var path = p.join(cjsonExamplePath, '/usr/local/lib');
+//   if (Platform.isMacOS) {
+//     path = p.join(path, 'libcjson.dylib');
+//   } else if (Platform.isWindows) {
+//     path = p.join(path, 'Debug', 'cjson.dll');
+//   } else {
+//     path = p.join(path, 'libglfw.so');
+//   }
+//   return path;
+// }
